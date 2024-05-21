bd39a0bd27e11786b75951f545bf7863f15307f9
Austin Adamson <austin@roguewolf.tech>
2024-05-20 22:58:30 -0700

    Running animations

---
+++ scripts/Player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 120.0
+const JUMP_VELOCITY = -400.0
+
+@onready var sprite = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_dir = Input.get_axis("move_left", "move_right")
+	if x_dir:
+		sprite.play("move_right")
+		if x_dir > 0:
+			sprite.flip_h = false
+		elif x_dir < 0: 
+			sprite.flip_h = true
+			
+		velocity.x = x_dir * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	
+	var y_dir = Input.get_axis("move_up", "move_down")
+	if y_dir:
+		if x_dir == 0 && y_dir > 0:
+			sprite.play("run_down")
+		elif x_dir == 0 && y_dir < 0: 
+			sprite.play("run_up")
+			
+		velocity.y = y_dir * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if x_dir == 0 && y_dir == 0: 
+		sprite.play("idle")
+
+	move_and_slide()
